f501870653ffda4c93393d40e6f0ae4f87ce2073
Julia-08 <julia.d.paula@aluno.senai.br>
2025-06-10 17:26:56 -0300

    lista de tarefas funcional

--- scritp.js
+++ scritp.js
@@ -1,14 +1,38 @@
 // definir constantes para uso no codigo
-const btnadd = document.getElementById('btn-add');
+const btnAdd = document.getElementById('btnAdd');
 const tarefa = document.getElementById('tarefa');
-const tasklist = document.getElementById('taksliste');
+const tasklist = document.getElementById('tasklist');
 const titulo = document.getElementById('titulo');
 
 let nome = prompt("Qual o seu nome?");
 titulo.innerHTML = `Lista de Tarefa: ${nome}`
 
-btnadd.addEventListener("click", criaTarefa);
+btnAdd.addEventListener("click",criaTarefa);
 
 function criaTarefa(){
+    const listItem = document.createElement('li')
+    listItem.textContent = tarefa.value;
+    tasklist.appendChild(listItem);
+
+    const removeButton = document.createElement('button');
+    removeButton.id = "remove"
+    removeButton.textContent = "X"
+
+    removeButton.addEventListener("click", function(){
+        tasklist.removeChild(listItem);
+    })
+
+    const concluirButton = document.createElement('button');
+    concluirButton.id = "conclui"
+    concluirButton.textContent = "✓"
+
+    let buttonsItem = document.createElement('div');
+    buttonsItem.classList.toggle('buttonsItem')
+    listItem.appendChild(buttonsItem);
+    buttonsItem.appendChild(concluirButton);
+    buttonsItem.appendChild(removeButton);
     
+    concluirButton.addEventListener("click", function(){
+        listItem.classList.toggle('completed');
+    })
 }
